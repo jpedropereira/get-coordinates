aba20eb96f0aaddc74229f25745902d1eaa45bde
Joao Pereira <84179710+jpedropereira@users.noreply.github.com>
2022-06-02 17:11:02 +0200

    Added connection to workbook and worksheet containing the addresses list

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ from geopy.geocoders import Bing
 load_dotenv()
 
 BING_API_KEY = os.getenv("BING_API_KEY")
+WB_PATH = "addresses_template.xlsx" #path to the file containing the addresses
+
+locator = Bing(api_key=BING_API_KEY)
+
+#builds connection to workbook containing the addresses
+wb = xw.Book(WB_PATH)
+#builds connection to worksheer containing the addresses
+ws = wb.sheets[0]
+
